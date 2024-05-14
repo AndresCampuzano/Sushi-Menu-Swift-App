b4b2e37b3ad5782e6b3235c7044743d5f972d312
AndresCampuzano <campuxano88@gmail.com>
2024-05-14 11:28:46 -0500

    Initial Commit

---
+++ swift-menu/swift_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_menuApp.swift
+//  swift-menu
+//
+//  Created by Andres C G on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
